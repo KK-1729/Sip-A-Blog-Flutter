43b46df7c014e4aae3d02419cd011fbe0f27131a
sathvikallam <sathvikrddy@gmail.com>
2021-08-11 00:21:58 +0530

    added screen2

---
+++ lib/Screens/Welcome.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class Welcome extends StatelessWidget {
+  const Welcome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(child : Text('welcome'));
+  }
+}
