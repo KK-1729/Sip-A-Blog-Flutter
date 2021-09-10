a171f7bdc020157a1093dbb7e50e7aee064ddffb
sathvikallam <sathvikrddy@gmail.com>
2021-09-11 03:19:07 +0530

    Final

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
 
   @override
+
   Widget build(BuildContext context) {
-    return MaterialApp(home: Mainbuild());
+    return MaterialApp(
+        home: Mainbuild());
+
   }
 }
 
@@ -27,54 +30,58 @@ class Mainbuild extends StatefulWidget {
 
 class _MainbuildState extends State<Mainbuild> {
   int _selectedIndex = 0;
-  static const List<Widget> Pagelist = [Welcome(), Blogspage(), About(), Contact()];
-  void switchindex(int index) {
+  static const List<Widget> Pagelist =  [
+    Welcome(),
+    Blogspage(),
+    About(),
+    Contact()
+  ];
+  void switchindex (int index) {
     setState(() {
       _selectedIndex = index;
     });
   }
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          body: Pagelist[_selectedIndex],
-          bottomNavigationBar: Theme(
-            data: ThemeData.from(colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue)),
-            // data: ,
-            child: BottomNavigationBar(
-              unselectedItemColor: Colors.black,
-              items: [
-                BottomNavigationBarItem(
-                    // backgroundColor: Colors.red,
-                    icon: Icon(Icons.ad_units),
-                    label: 'Welcome'),
-                BottomNavigationBarItem(
-                  //backgroundColor: Colors.blue,
-                  icon: Icon(Icons.photo_album),
-                  label: 'Blogs',
-                ),
-                BottomNavigationBarItem(
-                  //backgroundColor: Colors.green,
-                  icon: Icon(Icons.book),
-                  label: 'About',
-                ),
-                BottomNavigationBarItem(
-                  //backgroundColor: Colors.pink,
-                  icon: Icon(Icons.contact_page),
-                  label: 'Contact',
-                ),
-              ],
-              currentIndex: _selectedIndex,
-              selectedItemColor: Colors.amber[800],
-              onTap: (int index) => setState(() {
-                print(_selectedIndex);
-                // _selectedIndex = index;
-                switchindex(index);
-              }),
+        body: IndexedStack(
+            index: _selectedIndex,
+            children: Pagelist),
+        bottomNavigationBar: BottomNavigationBar(
+          unselectedItemColor: Colors.black,
+          items: [
+            BottomNavigationBarItem(
+              // backgroundColor: Colors.red,
+                icon: Icon(Icons.ad_units),
+                label: 'Welcome'
             ),
-          )),
+            BottomNavigationBarItem(
+              //backgroundColor: Colors.blue,
+              icon: Icon(Icons.photo_album),
+              label: 'Blogs',
+            ),
+            BottomNavigationBarItem(
+              //backgroundColor: Colors.green,
+              icon: Icon(Icons.book),
+              label: 'About',
+            ),
+            BottomNavigationBarItem(
+              //backgroundColor: Colors.pink,
+              icon: Icon(Icons.contact_page),
+              label: 'Contact',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.amber[800],
+          onTap: (int index) => setState(() {
+            print(_selectedIndex);
+            // _selectedIndex = index;
+            switchindex(index);
+          }),
+        ),
+
+      ) ,
       // initialRoute: About.id,
       // routes: {
       //   Welcome.id: (context) => Welcome(),
@@ -82,10 +89,11 @@ class _MainbuildState extends State<Mainbuild> {
       //   About.id: (context) => About(),
       //   Contact.id: (context) => Contact(),
       // },
-    );
-    ;
+    );;
   }
 }
+
+
 
 // syntax to go to a particular screen: (example: About screen)
 //  onpressed: () {
